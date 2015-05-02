50430f1d710f65cb9025fbb105338f05ceb27303
Aaron Lee <aaronlee.ucla@gmail.com>
2015-05-02 14:16:10 -0400

    test

--- src/main/java/cs757/project/RemoveRareMovies.java
+++ src/main/java/cs757/project/RemoveRareMovies.java
@@ -5,70 +5,83 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
-
+import java.util.TreeSet;
 
 public class RemoveRareMovies {
+	
+	private static final String SEPARATOR = "::";
 	
     public static void main(String[] args) throws IOException{
         
     	ClassLoader classLoader = RemoveRareMovies.class.getClassLoader();
-		File file = new File(classLoader.getResource("10M/updated_ratings.txt").getFile());
+		File file = new File(classLoader.getResource("10M/ratings.dat").getFile());
 		
 		HashMap<String, Integer> map = new HashMap<String,Integer>();
-		double ratings = 0;
-		
+		double numOfRatings = 0;
 		Scanner scanner = new Scanner(file);
+		
 		while (scanner.hasNextLine()) {
-			ratings++;
+			numOfRatings++;
 			String line = scanner.nextLine();
-		 	String movieId = line.split("::")[1];
+		 	String movieId = line.split(SEPARATOR)[1];
 		 	Integer prev = map.put(movieId, 1);
 		 	if ( prev != null )
 		 		map.put(movieId, prev+1);
 		}
 		scanner.close();
 		
-		Collection<Integer> counts = map.values();
-		ArrayList<Integer> ordered = new ArrayList<Integer>(counts);
+		ArrayList<Integer> ordered = new ArrayList<Integer>(map.values());
 		Collections.sort(ordered);
 		
-		System.out.println("size="+ordered.size());
-		int increments = ordered.size()/20;
-		
-		int cutoff = 0;
+//		System.out.println("size="+ordered.size());
 		int percentile = 35;
-		for ( int i = 1; i < 20; i++ ){
-			System.out.println("percentile="+i*5+", counts="+ordered.get(i*increments));
-			if ( i*5 == percentile )
-				cutoff = ordered.get(i*increments);
-		}
+		int cutoff = ordered.get((ordered.size()*percentile)/100);
+//		System.out.println("cutoff="+cutoff);
 		
 		double ratingsOnRareMovies = 0.0;
+		TreeSet<Integer> reducedMovies = new TreeSet<Integer>();
+		
 		scanner = new Scanner(file);
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+		 	String movieId = line.split(SEPARATOR)[1];
+		 	Integer ratingsCount = map.get(movieId);
+		 	if ( ratingsCount <= cutoff )
+		 		ratingsOnRareMovies++;
+		 	else
+		 		reducedMovies.add(Integer.valueOf(movieId));
+		}
+		scanner.close();
+		System.out.println("By removing bottom "+percentile+" percentile of movies, M loses "+(100*ratingsOnRareMovies)/numOfRatings + " percent of ratings");
+		
+		HashMap<String, Integer> idMap = new HashMap<String, Integer>();
+		Integer newId = 1;
+		for ( Integer movieId : reducedMovies )
+			idMap.put(String.valueOf(movieId), newId++);
+		
 		File outputFile = new File("reduced_ratings.txt");
 		if (!outputFile.exists()) 
 			outputFile.createNewFile();
 		FileWriter fw = new FileWriter(outputFile.getAbsoluteFile());
 		BufferedWriter bw = new BufferedWriter(fw);
 		
+		scanner = new Scanner(file);
 		while (scanner.hasNextLine()) {
 			String line = scanner.nextLine();
-		 	String movieId = line.split("::")[1];
-		 	Integer numOfRatings = map.get(movieId);
-		 	if ( numOfRatings <= cutoff )
-		 		ratingsOnRareMovies++;
-		 	else {
-		 		bw.write(line);
+		 	String tokens[]= line.split(SEPARATOR);
+		 	String movieId = tokens[1];
+		 	Integer ratingsCount = map.get(movieId);
+		 	if ( ratingsCount > cutoff ) {
+		 		Integer updatedId = idMap.get(movieId);
+		 		bw.write(tokens[0]+"::"+updatedId+"::"+tokens[2]);
 			 	bw.newLine();
-		 	}
+			}
 		}
 		scanner.close();
 		bw.close();
-		
-		System.out.println("By removing bottom "+percentile+" percentile of movies, M loses "+(100*ratingsOnRareMovies)/ratings + " percent of ratings");
+	
     }
 }
